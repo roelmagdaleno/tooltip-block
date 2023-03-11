03df9120c0baa4707c571fa24e42950fcce14703
Roel Magdaleno <roelmagdaleno@gmail.com>
2023-03-10 19:45:28 -0600

    Minimal changes

--- tooltip-block.php
+++ tooltip-block.php
@@ -3,9 +3,9 @@
  * Plugin Name:       Tooltip Block
  * Description:       Example block scaffolded with Create Block tool.
  * Requires at least: 6.1
- * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Requires PHP:      7.4
+ * Version:           1.0.0
+ * Author:            Roel Magdaleno
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       tooltip-block
@@ -15,12 +15,13 @@
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
+ * Behind the scenes, it registers also all assets, so they can be enqueued
  * through the block editor in the corresponding context.
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function create_block_tooltip_block_block_init() {
+function create_tooltip_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
-add_action( 'init', 'create_block_tooltip_block_block_init' );
+
+add_action( 'init', 'create_tooltip_block_init' );
